0bb943e22f675be80b9f9572c02844c19938914d
Lars Grammel <lars.grammel@gmail.com>
2023-02-07 14:13:24 +0100

    List git files in repository-path location.

--- src/RepositoryIndexCLI.ts
+++ src/RepositoryIndexCLI.ts
@@ -1,11 +1,31 @@
+import { Option } from "commander";
+import { simpleGit } from "simple-git";
+import zod from "zod";
 import { runProgram } from "./program/runProgram";
-import zod from 'zod';
 
 runProgram({
-  name: 'Repository Index',
-  configurationOptions: [ ],
-  configurationSchema: zod.object({}),
-  async run({}) {
-    console.log('Hello, world!');
-  }
+  name: "Repository Index",
+  configurationOptions: [
+    new Option(
+      "--repository-path <string>",
+      "Git repository path"
+    ).makeOptionMandatory(),
+  ],
+  configurationSchema: zod.object({
+    repositoryPath: zod.string(),
+  }),
+  async run({ repositoryPath }) {
+    const git = simpleGit({
+      baseDir: repositoryPath,
+      binary: "git",
+      maxConcurrentProcesses: 6,
+      trimmed: false,
+    });
+
+    const files = (await git.raw(["ls-files"])).split("\n");
+
+    for (const file of files) {
+      console.log(file);
+    }
+  },
 });
