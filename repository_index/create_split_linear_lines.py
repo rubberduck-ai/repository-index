87caf692e04acfa6bf3a29c2801501b840a74cb9
Lars Grammel <lars.grammel@gmail.com>
2023-02-07 22:15:19 +0100

    Use pythonic variable names.

--- repository_index/create_split_linear_lines.py
+++ repository_index/create_split_linear_lines.py
@@ -1,62 +1,62 @@
-def calculateLinePositions(lines, lineSeparator):
-    linePositions = []
+def calculate_line_positions(lines, line_separator):
+    line_positions = []
 
     position = 0
 
     for line in lines:
-        linePositions.append(
+        line_positions.append(
             {
                 "start": position,
                 "end": position + len(line),  # note: separator is not included
             }
         )
 
-        position += len(line) + len(lineSeparator)
+        position += len(line) + len(line_separator)
 
-    return linePositions
+    return line_positions
 
 
-def createSplitLinearLines(maxChunkCharacters, lineSeparator="\n"):
-    def splitLinearLines(content):
-        lines = content.split(lineSeparator)
-        linePositions = calculateLinePositions(lines, lineSeparator)
+def create_split_linear_lines(max_chunk_characters, line_separator="\n"):
+    def split_linear_lines(content):
+        lines = content.split(line_separator)
+        line_positions = calculate_line_positions(lines, line_separator)
 
         chunks = []
 
         segment = None
 
-        def addSegmentToChunks(currentLine):
+        def add_segment_to_chunks(current_line):
             nonlocal segment
             if segment == None:
                 return
             chunks.append(
                 {
-                    "startPosition": linePositions[segment["startLine"]]["start"],
-                    "endPosition": linePositions[currentLine]["end"],
-                    "content": lineSeparator.join(segment["lines"]),
+                    "startPosition": line_positions[segment["startLine"]]["start"],
+                    "endPosition": line_positions[current_line]["end"],
+                    "content": line_separator.join(segment["lines"]),
                 }
             )
             segment = None
 
         for i in range(len(lines)):
-            lineText = lines[i]
+            line_text = lines[i]
 
             if segment == None:
                 segment = {
-                    "lines": [lineText],
+                    "lines": [line_text],
                     "startLine": i,
-                    "characterCount": len(lineText),
+                    "characterCount": len(line_text),
                 }
             else:
-                segment["lines"].append(lineText)
-                segment["characterCount"] += len(lineText) + len(lineSeparator)
+                segment["lines"].append(line_text)
+                segment["characterCount"] += len(line_text) + len(line_separator)
 
             # this leads to chunks that are too big (by 1 line)
-            if segment["characterCount"] > maxChunkCharacters:
-                addSegmentToChunks(i)
+            if segment["characterCount"] > max_chunk_characters:
+                add_segment_to_chunks(i)
 
-        addSegmentToChunks(len(lines) - 1)
+        add_segment_to_chunks(len(lines) - 1)
 
         return chunks
 
-    return splitLinearLines
+    return split_linear_lines
