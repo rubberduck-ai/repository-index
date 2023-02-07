3d2b5ed7a3c0be0b0aa715b2d179f04d66e2b71a
Lars Grammel <lars.grammel@gmail.com>
2023-02-07 22:32:25 +0100

    Move into separate function.

--- repository_index/repository_index.py
+++ repository_index/repository_index.py
@@ -1,44 +1,7 @@
 import argparse
+
+from is_supported_file import is_supported_file
 from git import Repo
-
-
-def is_valid_file(filename):
-    return (
-        filename.endswith(".ts")
-        or filename.endswith(".js")
-        or filename.endswith(".tsx")
-        or filename.endswith(".sh")
-        or filename.endswith(".py")
-        or filename.endswith(".md")
-        or filename.endswith(".json")
-        or filename.endswith(".yml")
-        or filename.endswith(".yaml")
-        or filename.endswith(".html")
-        or filename.endswith(".css")
-        or filename.endswith(".scss")
-        or filename.endswith(".less")
-        or filename.endswith(".xml")
-        or filename.endswith(".java")
-        or filename.endswith(".cs")
-        or filename.endswith(".cpp")
-        or filename.endswith(".c")
-        or filename.endswith(".h")
-        or filename.endswith(".go")
-        or filename.endswith(".php")
-        or filename.endswith(".rb")
-        or filename.endswith(".swift")
-        or filename.endswith(".kt")
-        or filename.endswith(".dart")
-        or filename.endswith(".sql")
-        or filename.endswith(".graphql")
-        or filename.endswith(".gql")
-        or filename.endswith(".txt")
-    ) and not (
-        filename.endswith(".min.js")
-        or filename.endswith(".min.css")
-        or filename.endswith("pnpm-lock.yaml")
-    )
-
 
 parser = argparse.ArgumentParser(description="Repository Index")
 
@@ -56,7 +19,7 @@ repo = Repo(args.repository_path)
 allFiles = repo.git.ls_files().split("\n")
 
 
-result = list(filter(is_valid_file, allFiles))
+result = list(filter(is_supported_file, allFiles))
 
 # read the content of each file
 for file in result:
