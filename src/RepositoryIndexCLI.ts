d0dbeeac22fca692288f74c3cef4e70bf5c00595
Lars Grammel <lars.grammel@gmail.com>
2023-02-07 14:41:21 +0100

    Add file exclusions. Create chunks with embedding.

--- src/RepositoryIndexCLI.ts
+++ src/RepositoryIndexCLI.ts
@@ -2,6 +2,7 @@ import { Option } from "commander";
 import fs from "node:fs/promises";
 import { simpleGit } from "simple-git";
 import zod from "zod";
+import { Chunk } from "./chunk/Chunk";
 import { createSplitLinearLines } from "./chunk/splitLinearLines";
 import { runProgram } from "./program/runProgram";
 
@@ -26,6 +27,13 @@ runProgram({
 
     const files = (await git.raw(["ls-files"])).split("\n");
 
+    const chunksWithEmbedding: Array<
+      Chunk & {
+        file: string;
+        embedding: Array<number>;
+      }
+    > = [];
+
     for (const file of files) {
       if (!isSupportedFile(file)) {
         continue;
@@ -38,24 +46,35 @@ runProgram({
       })(content);
 
       for (const chunk of chunks) {
-        console.log(chunk);
+        chunksWithEmbedding.push({
+          file,
+          ...chunk,
+          embedding: [],
+        });
       }
     }
+
+    console.log(chunksWithEmbedding);
   },
 });
 
 function isSupportedFile(file: string) {
   return (
-    file.endsWith(".js") ||
-    file.endsWith(".ts") ||
-    file.endsWith(".tsx") ||
-    file.endsWith(".sh") ||
-    file.endsWith(".yaml") ||
-    file.endsWith(".yml") ||
-    file.endsWith(".md") ||
-    file.endsWith(".css") ||
-    file.endsWith(".json") ||
-    file.endsWith(".toml") ||
-    file.endsWith(".config")
+    (file.endsWith(".js") ||
+      file.endsWith(".ts") ||
+      file.endsWith(".tsx") ||
+      file.endsWith(".sh") ||
+      file.endsWith(".yaml") ||
+      file.endsWith(".yml") ||
+      file.endsWith(".md") ||
+      file.endsWith(".css") ||
+      file.endsWith(".json") ||
+      file.endsWith(".toml") ||
+      file.endsWith(".config")) &&
+    !(
+      file.endsWith(".min.js") ||
+      file.endsWith(".min.css") ||
+      file.endsWith("pnpm-lock.yaml")
+    )
   );
 }
