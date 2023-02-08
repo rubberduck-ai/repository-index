e150cce855e32d506abe33cb08963b50f0245895
Lars Grammel <lars.grammel@gmail.com>
2023-02-08 14:59:44 +0100

    Cleanup & docs.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Rubberduck Repository Index
 
 Indexes a Git repository using embeddings.
+
+## Setup
+
+`pip install -r requirements_dev.txt`
+
+## Run
+
+`python3 repository_index/repository_index.py --repository-path . --output-file result.json`
+
+Needs `OPENAI_API_KEY` environment variable to be set.
--- repository_index/repository_index.py
+++ repository_index/repository_index.py
@@ -12,12 +12,8 @@ parser.add_argument(
     "--repository-path", type=str, help="Git repository path", required=True
 )
 parser.add_argument("--output-file", type=str, help="Output file", required=True)
-parser.add_argument("--open-ai-api-key", type=str, help="OpenAI API key", required=True)
 
 args = parser.parse_args()
-
-# Using env param instead of this
-# openai.api_key = args.open_ai_api_key
 
 repo = Repo(args.repository_path)
 allFiles = repo.git.ls_files().split("\n")
